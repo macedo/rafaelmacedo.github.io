6bf72f51faa764ce4a2d8a4e515b1963b2981969
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-05-21 00:41:08 -0300

    add plugin to localize strings

---
+++ _plugins/i18n_filter.rb
@@ -0,0 +1,23 @@
+require "i18n"
+
+LOCALE = :"pt-BR"
+
+module Jekyll
+  module I18nFilter
+
+    def localize(input, format=nil)
+      load_translations
+      format = (format =~ /^:(\w+)/) ? $1.to_sym : format
+      I18n.l input, format: format
+    end
+
+    def load_translations
+      unless I18n::backend.instance_variable_get(:@translations)
+        I18n.backend.load_translations Dir[File.join(File.dirname(__FILE__), "../_locales/*.yml")]
+        I18n.locale = LOCALE
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::I18nFilter)
