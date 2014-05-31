ab2d13f7d170a3cc33167407b994a9a327532be8
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-05-31 12:59:47 -0300

    load disqus after all page loaded

--- _includes/js/async_gist_loader.js
+++ _includes/js/async_gist_loader.js
@@ -1,17 +1,10 @@
 <script>
-(function() {
-  function asyncLoad() {
+    (function() {
     var script = document.createElement("script");
     script.src = "/assets/js/async-gist.js";
     script.async = true
 
     var entry = document.getElementsByTagName("script")[0];
     entry.parentNode.insertBefore(script, entry);
-  }
-
-  if (window.attachEvent)
-    window.attachEvent("onload", asyncLoad);
-  else
-    window.addEventListener("load", asyncLoad, false);
-})();
+  })();
 </script>
